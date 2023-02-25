b8eee8aa94d3a41d640b8063019998a552eb134c
German De Francesco <germandefrancesco@hotmail.com>
2023-02-25 12:53:28 -0300

    Add Login page and Cookie Auth Handler

---
+++ AspNetCoreIdentityCourse/Pages/Account/Login.cshtml.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Claims;
+
+namespace AspNetCoreIdentityCourse.Pages.Account;
+
+public class LoginModel : PageModel
+{
+    [BindProperty]
+    public Credential Credential { get; set; } = new();
+
+    public void OnGet()
+    {
+    }
+
+    public async Task<IActionResult> OnPostAsync()
+    {
+        if (!ModelState.IsValid || Credential.UserName != "admin" || Credential.Password != "password")
+        {
+            return Page();
+        }
+        
+        var claims = new List<Claim>
+        {
+            new Claim(ClaimTypes.Name, "admin"),
+            new Claim(ClaimTypes.Email, "admin@mywebsite.com")
+        };
+        var identity = new ClaimsIdentity(claims, "MyCookieAuth");
+        var claimsPrincipal = new ClaimsPrincipal(identity);
+
+        await HttpContext.SignInAsync("MyCookieAuth", claimsPrincipal);
+
+        return RedirectToPage("/Index");
+    }
+}
+
+public class Credential
+{
+    [Required]
+    [Display(Name = "User Name")]
+    public string UserName { get; set; } = "";
+
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; } = "";
+}
